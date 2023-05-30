6910cf95ddd7e1057a8de582d8b87f2a49d46e23
Bruno <brunolutterbach13@gmail.com>
2023-05-30 20:10:37 -0300

    Add StatusIngresso no retorno das informacoes

--- src/main/java/br/com/brunolutterbach/ticketmasterapi/model/ingresso/DadosIngressosUsuario.java
+++ src/main/java/br/com/brunolutterbach/ticketmasterapi/model/ingresso/DadosIngressosUsuario.java
@@ -1,6 +1,7 @@
 package br.com.brunolutterbach.ticketmasterapi.model.ingresso;
 
 import br.com.brunolutterbach.ticketmasterapi.model.enums.StatusEvento;
+import br.com.brunolutterbach.ticketmasterapi.model.enums.StatusIngresso;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public record DadosIngressosUsuario(
+        StatusIngresso statusIngresso,
         String nome,
         String descricao,
         LocalDate dataEvento,
