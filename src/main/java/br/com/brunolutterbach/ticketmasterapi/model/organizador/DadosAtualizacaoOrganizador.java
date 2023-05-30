7c2a6867b931b5ed8f79a6803588677c97f9be05
Bruno <brunolutterbach13@gmail.com>
2023-05-30 20:11:11 -0300

    Record para atualizacao do Organizador

---
+++ src/main/java/br/com/brunolutterbach/ticketmasterapi/model/organizador/DadosAtualizacaoOrganizador.java
@@ -0,0 +1,12 @@
+package br.com.brunolutterbach.ticketmasterapi.model.organizador;
+
+
+public record DadosAtualizacaoOrganizador(
+        String nomeEmpresa,
+        String email,
+        String site,
+        String cnpj,
+        String telefone
+
+) {
+}
